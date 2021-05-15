126df56fd46a4a98d6191d398ebb93b468198498
SashaCN <sashacn228@gmail.com>
2021-05-15 21:59:49 +0300

    part of slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,3 +9,31 @@ function menuToggle (event){
   header.classList.toggle("active-header")
   burger.classList.toggle("active-burger")
 }
+
+// gallery slider 
+let sliderLine = document.querySelector(".slider-line"),
+    left = document.querySelector(".arr-left"),
+    right = document.querySelector(".arr-right"),
+    img = document.querySelectorAll(".slider-line img"),
+    slideWidth = img[1].getBoundingClientRect().width
+
+left.onclick = slideLeft 
+right.onclick = slideRight 
+
+function slideLeft (event){
+  event.preventDefault() 
+  if(sliderLine.scrollLeft != 0){
+    console.log(slideWidth)
+    sliderLine.scrollLeft = sliderLine.scrollLeft - slideWidth
+  }else{
+    sliderLine.scrollLeft = sliderLine.scrollWidth - slideWidth
+  }
+}
+function slideRight (event){
+  event.preventDefault() 
+  if(sliderLine.scrollLeft + slideWidth >= sliderLine.scrollWidth - 10 && sliderLine.scrollLeft + slideWidth <= sliderLine.scrollWidth + 10){
+    sliderLine.scrollLeft = 0
+  }else{
+    sliderLine.scrollLeft = sliderLine.scrollLeft + slideWidth
+  }
+}
